9cc700b6191fd3de6cf164840ddd822ee2c25848
hardworkingmission <shakil20131101041@gmail.com>
2022-02-15 23:01:55 +0600

    project-done

---
+++ index.js
@@ -0,0 +1,39 @@
+
+//getInput
+function getInput(inputId){
+    return document.getElementById(inputId)
+}
+
+//getDigit
+
+function getDigit(num){
+    if(num=="MC"){ 
+        getInput('digit').innerText='0'
+        getInput('result').innerText='0'
+    }else if(num !='M+'){
+        getInput('digit').innerText+=num
+    }
+          
+}
+//doCalculation
+function doCalculation(){
+    const stringArray=getInput('digit').innerText.replace(/[^-()\d/*+.]/g, '');
+    
+    return eval(stringArray)
+
+}
+    
+getInput('key-pad').addEventListener('click',(event)=>{
+    if(event.target.innerText !='='&& event.target.getAttribute('id')!='key-pad'){
+        getDigit(event.target.innerText)
+    }
+    console.log(event.target)
+
+})
+
+getInput('equal').addEventListener('click',(event)=>{
+    event.stopPropagation()
+    getInput('result').innerText=doCalculation()
+    
+    //console.log("equal",event.target.innerText)
+})
